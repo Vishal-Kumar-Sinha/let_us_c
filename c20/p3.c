905b794f6708b7345f7e3e4d2046421bc806dcdb
VISHAL KUMAR SINHA <vishalsinha211299@gmail.com>
2024-05-24 11:05:54 +0530

    l

--- c20/p3.c
+++ c20/p3.c
@@ -1,40 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX 10000
+#define EndOfWord (word[i]==' '||word[i]==','||word[i]=='\n'||word[i]=='.')
 
-#define EndOfWord word[i] == ' ' || word[i] == ',' || word[i] == '\n' || word[i] == '.'
-
-int main(int ac, char *av[]) {
-    FILE *fp, *temp;
-    int i, j;
-    char word[10], ch;
+int main(int ac, char *argv[]) {
+    FILE *fp;
     if (ac != 4) {
-        puts("Wrong number of arguements\n.");
+        puts("Wrong number of arguements\n");
         printf("Write like this : ");
         printf("\"change\" \"old_word\" \"new_word\" \"file_name\"\n");
         exit(1);
     }
-    fp = fopen(av[3], "r");
-    temp = fopen("temp.txt", "w");
+    char *oldWord = argv[1];
+    char *newWord = argv[2];
+    char *filename = argv[3];
+    fp = fopen(argv[3], "r");
     if (fp == NULL) {
         perror("Error : ");
         exit(1);
     }
-    for (i = 0; (word[i] = fgetc(fp)) != EOF; i++) {
-        if (EndOfWord) {
-            ch = word[i];
-            word[i] = '\0';
-            if (!strcmp(av[1], word))
-                fputs(av[2], temp);
-            else
-                fputs(word, temp);
-            fputc(ch, temp);
-            i = -1;
+    char buffer[MAX];
+    int oldWordLen=strlen(oldWord),newWordLen=strlen(newWord),bytesRead;
+    while((bytesRead=fread(buffer,1,MAX,fp))>0) {
+        for(int i=0;i<bytesRead-oldWordLen;i++) {
+            if(strncmp(buffer+i,oldWord,oldWordLen)==0) {
+                memcpy(buffer+i,newWord,newWordLen);
+                i+=newWordLen-1;
+            }
         }
+        fwrite(buffer,1,bytesRead,stdout);
     }
     fclose(fp);
-    fclose(temp);
-    remove(av[3]);
-    rename("temp.txt", av[3]);
     return 0;
+
 }
